26c6fdf71807506b6c112fc8fad41d70fe053821
mrshaktilearning5 <mrshaktilearning5@gmail.com>
2024-08-13 22:59:53 +0530

    initial project commit

---
+++ multithreading/src/main/java/com/example/multithreading/multithreading/MultithreadingApplication.java
@@ -0,0 +1,13 @@
+package com.example.multithreading.multithreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadingApplication.class, args);
+	}
+/Applications/apache-maven-3.8.6
+}
